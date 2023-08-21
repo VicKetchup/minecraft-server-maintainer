26a3fe53064eab7ee279fae4a3564391f793c0d6
VicKetchup <43671684+VicKetchup@users.noreply.github.com>
2023-08-21 04:40:49 +0100

    Licensing

--- maintainer-modules/maintainer-23-java-updater.sh
+++ maintainer-modules/maintainer-23-java-updater.sh
@@ -138,3 +138,17 @@ else
     fi
     skipSuccessLog=false
 fi
+# This file is part of Minecraft Server-Maintainer.
+#
+# Minecraft Server-Maintainer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Minecraft Server-Maintainer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Minecraft Server-Maintainer.  If not, see <https://www.gnu.org/licenses/>.
