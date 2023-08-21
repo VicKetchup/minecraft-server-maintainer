9d44853f4c18b6ffb776133d3ad3eee4379f1532
VicKetchup <43671684+VicKetchup@users.noreply.github.com>
2023-08-21 09:46:40 +0100

    Startup cleanup

--- Ketchup_and_Co_startup.sh
+++ Ketchup_and_Co_startup.sh
@@ -2,115 +2,9 @@
 printServerInfo=true
 clearForFrames=false
 export "isMaintainerRun"="true"
+export "tmuxName"="prison"
 source maintainer-common.sh clearForFrames=$clearForFrames
 sleep 1
-function getVisibleLength() {
-    local string="$1"
-    # echo "getVisibleLength: input string: $string" >&2
-    # Remove all escape sequences
-    string=$(echo -n "$string" | sed -r 's/(\\e|\\033)\[[0-9;]*[a-zA-Z]//g')
-    # Remove newline characters and literal \n sequences
-    string="${string//$'\n'}"
-    string="${string//\\n}"
-    # echo "getVisibleLength: string after removing escape sequences, newline characters, and literal \\n sequences: $string" >&2
-    # Count the remaining characters
-    local visibleLength=$(echo -n "$string" | wc -m)
-    # echo "getVisibleLength: visible length: $visibleLength" >&2
-    echo "$visibleLength"
-}
-
-# Function to center and print a given string with a blue background
-function centerAndPrintString() {
-    # Check if a shorter string was provided for use in case of a very slim window
-    if [[ -n $centerAndPrintStringShort ]] && [[ $(getVisibleLength "$1") -gt "$windowWidth" ]]; then
-        stringToCenter="$centerAndPrintStringShort"
-    else
-        stringToCenter="$1"
-    fi
-    windowWidth=$(tput cols)
-
-    # Extract the background color from the input string
-    backgroundColor=$(echo -n "$stringToCenter" | grep -oP '(?<=\\e\[)[0-9;]*m' | head -n 1)
-
-    # Calculate the visible string length (excluding escape sequences)
-    visibleStringToCenterLength=$(getVisibleLength "$stringToCenter")
-    if [ -n  "$leftEdgeSymbols" ]; then
-        visibleEdgesLength=$(getVisibleLength "${leftEdgeSymbols}${rightEdgeSymbols}")
-        visibleStringLength=$(( visibleStringToCenterLength + visibleEdgesLength ))
-    else
-        visibleStringLength=$(( visibleStringToCenterLength ))
-    fi
-
-    # Calculate the spaces needed for centering
-    centeringSpacesLength=$(( (windowWidth - visibleStringLength) / 2 ))
-
-    # Print the background color for the entire line
-    printf "\033[${backgroundColor}"
-    printf "${leftEdgeSymbols}"
-    for ((i = 0; i < windowWidth; i++)); do
-        printf " "
-    done
-    if [ -z  "$leftEdgeSymbols" ]; then
-        printf "\n"
-    fi
-    # Move the cursor back up one line
-    tput cuu1
-
-    # Move the cursor to the right to center the string
-    if [[ $centeringSpacesLength -gt 0 ]]; then
-        printf "%${centeringSpacesLength}s"
-    fi
-
-    # Print the centered string with escape sequences intact
-    printf "$stringToCenter"
-
-    endCenteringSpacesLength=$(( windowWidth - centeringSpacesLength - visibleStringLength ))
-    if [[ $endCenteringSpacesLength -gt 0 ]]; then
-        printf "%${endCenteringSpacesLength}s"
-    fi
-    printf "${rightEdgeSymbols}\033[0m"
-
-    ## Move the cursor to the next line
-    tput cud1
-
-    # Reset the color
-    tput sgr0
-
-    # Clear to end of line
-    tput el
-
-    # Reset the variables for the next string
-    unset stringToCenter
-    unset leftEdgeSymbols
-    unset rightEdgeSymbols
-}
-
-function printLogo() {
-    centerAndPrintString "\e[0m               ▄█▀▀▀▀▀▀█▄█\e[047m▀▒▒▒▒▒▒▒▒▀\e[0m█▄▄\e[0m"
-    centerAndPrintString "\e[0m                 \e[040m█         █\e[047m▒▒▒\e[0m         ▀██▀\e[0m"
-    centerAndPrintString "\e[0m                 \e[040m█         █▒    ██\e[0m☰☰☰☰☰☰█\e[0m"
-    centerAndPrintString "\e[0m                  ▀\e[040m█    ▒     █▄ █\e[0m   \e[044m▒☠ ▒█\e[0m"
-    centerAndPrintString "\e[0m                   \e[040m \e[047m▀\e[040m▄     ▄      ██\e[0m  \e[044m▒▒▒\e[047m█\e[0m"
-    centerAndPrintString "\e[0m    \e[043m▄\e[0m                \e[040m▄▀▒▀▄▄▄    ▒▒███\e[0m    ▄\e[047m▀\e[0m▀\e[047m█\e[0m"
-    centerAndPrintString "\e[0m         \e[041m▒█▒\e[0m             ▄\e[040m▀▒▒▒▒▒ ▀▄▀▒   ▒▒▀█████▒▒██▄\e[0m   ▒"
-    centerAndPrintString "\e[0m       ▄\e[041m█ █\e[0m▄          ▄\e[041m█▒▒▒▒▒▒▒▒▒▒\e[040m█▒▒    ▒▒▒██▒▒\e[041m▀▀\e[43m▀▀\e[0m▄▄▄"
-    centerAndPrintString "\e[0m    \e[041m█▒ ▒█\e[0m         ▄\e[041m▀            █\e[0m█▒▒▒▒▒▀████████ \e[0m"
-    centerAndPrintString "\e[0m   \e[041m█▒  ▒█\e[0m       ▄\e[041m▀                 \e[040m██▒▒▒▒▒█ ▒ ▒ █\e[0m"
-    centerAndPrintString "\e[0m   \e[041m█▒  ▒\e[0m█▀      \e[041m█ \e[037mKetchup&Co. █      \e[0m██ █ █      █ \e[0m"
-    centerAndPrintString "\e[0m  \e[041m█▒▒ ▒█\e[0m      \e[041m█             █\e[0m \e[41m█      \e[040m█  \e[0m██      █\e[0m"
-    centerAndPrintString "\e[0m  \e[041m█▒▒\e[0m \e[41m▒█\e[0m      \e[041m█             █\e[0m  \e[041m█      \e[040m█   \e[040m█    █\e[0m "
-    centerAndPrintString "\e[0m  \e[040m█\e[41m▒\e[0m   █    \e[41m█              █\e[040m   \e[041m█▒▒▒▒▒▒█\e[040m   █   █\e[0m"
-    centerAndPrintString "\e[0m     \e[040m█    █\e[0m   \e[041m█             █\e[0m     \e[040m█      \e[040m▀█▄▀    █\e[0m "
-    centerAndPrintString "\e[0m    \e[040m█    █\e[0m   \e[041m█             █\e[0m      \e[040m█             █\e[0m"
-    centerAndPrintString "\e[0m     \e[040m█    █\e[0m   \e[041m█             █\e[0m▄\e[44m▀▀▀▀▀▀\e[040m▄▄▄           ▀▄\e[0m"
-    centerAndPrintString "\e[0m   \e[040m█    █\e[0m   \e[041m█              █\e[44m        ▀▀▀\e[040m▄▄▄▄▄▄▄▄▄▄▀\e[0m"
-    centerAndPrintString "\e[0m \e[040m█    █\e[0m   \e[041m█              ▀█\e[44m    ▒▄▄▄▄▄▄▀▀▀▀▀▀█\e[0m▄"
-    centerAndPrintString "\e[0m  \e[040m█    █\e[0m    \e[041m█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\e[44m█     ▀             █\e[0m"
-    centerAndPrintString "\e[0m \e[040m█    ▀\e[0m▄  ▄█\e[044m▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀       ▒            █\e[0m"
-    centerAndPrintString "\e[0m \e[040m▀▄    ▀▀▀\e[44m▒                   ▄▄\e[0m▀\e[44m█\e[44m▒         ▒█\e[0m"
-    centerAndPrintString "$footer"
-    sleep $1;
-}
 
 function tryWindowsDriveInfo() {
     disk_info=`powershell.exe -c "wmic diskdrive get model,size" | sed -e '1d' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | awk 'NF>0 {model=substr($0, 1, 25); size=substr($0, 26); printf "%-25s %5.0fGB\n", model, size/1024/1024/1024}'`
@@ -207,7 +101,7 @@ function getMaintainerScripts() {
     if [ ${#foundScripts[@]} -gt 0 ]; then
         availableMaintainerScripts=$( IFS=$'\n'; echo "${foundScripts[*]}" )
     fi
-    /bin/bash ${maintainerModulesPath}/maintainer-[0-9]*-server-status.sh colour=true
+    /bin/bash ${maintainerModulesPath}/maintainer-[0-9]*-server-status.sh colour=true tmuxName=$tmuxName serverFolder=$serverFolder
 }
 
 windowWidth=`tput cols`
@@ -227,3 +121,18 @@ if [ "$TERM_PROGRAM" != tmux ]; then
         centerAndPrintString "\e[46;30m Copy-Paste or Type to run :)"
     fi
 fi
+# minecraft-server-maintaner - Level up your Minecraft Server Maintanance and Control!
+# Copyright (C) 2023  Viktor Tkachuk, aka. VicKetchup, from Ketchup&Co.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
